9c125cb8f82dc0022636f27b0b569b56283e3f57
zwjddn1105 <zwjddn1105@naver.com>
2024-09-04 16:37:42 +0900

    [D2] Title: 풍선팡2, Time: 171 ms, Memory: 48,812 KB -BaekjoonHub

---
+++ SWEA/D2/16268. 풍선팡2/README.md
@@ -0,0 +1,15 @@
+# [D2] 풍선팡2 - 16268 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYYlGU56XOkDFARc) 
+
+### 성능 요약
+
+메모리: 48,812 KB, 시간: 171 ms, 코드길이: 710 Bytes
+
+### 제출 일자
+
+2024-09-04 16:37
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D2/16268. 풍선팡2/풍선팡2.py
@@ -0,0 +1,25 @@
+T = int(input())
+ 
+for tc in range(1, 1+T):
+    n,m = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
+    lst = []
+    for i in range(n):
+        for j in range(m):
+            pollen = arr[i][j]
+            sum_pollen = arr[i][j]
+            for k in range(4):
+                ni = i + dx[k]
+                nj = j + dy[k]
+                if 0<=ni<n and 0<=nj<m:
+                    sum_pollen += arr[ni][nj] 
+            lst.append(sum_pollen)
+    # lst 내에서 최대값만 출력하면 끝
+    result = lst[0]
+    for i in range(1, len(lst)):
+        if result < lst[i]:
+            result = lst[i]
+ 
+    print(f'#{tc} {result}')
