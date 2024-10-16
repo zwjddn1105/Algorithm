2f27ecd58389e9bda68910059536c3075c71bcc2
zwjddn1105 <zwjddn1105@naver.com>
2024-10-16 16:57:04 +0900

    [Bronze II] Title: 문자열 반복, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+for _ in range(T):
+    R, S = map(str, input().split())
+    R = int(R)
+    for i in S:
+        print(i*R, end='')
+    print()
