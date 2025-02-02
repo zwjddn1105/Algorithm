c83ec613b9b6d60b51276fdcb4939a581cf14d82
zwjddn1105 <zwjddn1105@naver.com>
2025-02-02 15:17:03 +0900

    [Silver IV] Title: 로프, Time: 276 ms, Memory: 26020 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.js
@@ -0,0 +1,13 @@
+const fs = require("fs")
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt"
+const [n, ...input] = fs.readFileSync(filePath).toString().trim().split("\n")
+
+let result = 0
+input.sort((a, b) => {
+  return Number(a.trim()) - Number(b.trim())
+})
+
+for (let i = 0; i <= n - 1; i++) {
+  result = Math.max(result, input[i] * (n - i))
+}
+console.log(result)
