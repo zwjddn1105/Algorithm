5000b5c7a68e500fcee9a68df37ef0ae1b1cfbd8
kimjeongwoo <zwjddn1105@naver.com>
2025-02-06 22:02:47 +0900

    BFS

---
+++ 이코테/DFS&BFS/BFS/BFS.py
@@ -0,0 +1,30 @@
+# BFS가 DFS보다 더 빠르게 동작한다는 점만 기억하자
+# 얘도 시간복잡도는 O(N)
+from collections import deque
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+def bfs(graph, start, visited):
+    visited[start] = 1
+    queue = deque([start])
+    # queue.append(start)
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = 1
+
+visited = [0] * 9
+
+bfs(graph, 1, visited)
