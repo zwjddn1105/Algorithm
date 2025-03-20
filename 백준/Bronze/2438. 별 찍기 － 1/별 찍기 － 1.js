c681cc25a70bda25dc6bed70495f61d3136b4658
김정우 <zwjddn1105@naver.com>
2025-03-20 09:25:02 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 100 ms, Memory: 9404 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const n = fs.readFileSync(filePath).toString().trim();
+
+let star = Number(n);
+
+for (let i = 0; i <= star - 1; i++) {
+  process.stdout.write("*".repeat(i + 1));
+  process.stdout.write("\n");
+}
