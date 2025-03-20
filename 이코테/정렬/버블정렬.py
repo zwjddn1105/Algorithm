45115e841ca3f4f5f943118cf5177463d2a4e9c9
zwjddn1105 <zwjddn1105@naver.com>
2025-03-20 21:33:18 +0900

    버블정렬.py

---
+++ 이코테/정렬/버블정렬.py
@@ -0,0 +1,12 @@
+def bubbleSort(arr):
+    n = len(arr) 
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]: 
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+arr = [64, 34, 25, 12, 57, 93, 1, 123]
+
+bubbleSort(arr)
+
+print(arr)
