0473b0ed6398e4af7d2415491056e0ee3f84beea
zwjddn1105 <zwjddn1105@naver.com>
2025-03-24 21:07:53 +0900

    시로코와 은행털기 풀이수정

--- 백준/Gold/26607. 시로코와 은행털기/시로코와 은행털기.py
+++ 백준/Gold/26607. 시로코와 은행털기/시로코와 은행털기.py
@@ -7,16 +7,16 @@ for i in range(n):
     arr[i] = a
 
 for p in arr:
-    for i in range(k-1, 0, -1):  # k-1부터 0까지
+    for i in range(k, 0, -1):  # k부터 1까지
         for j in range(x*k, p-1, -1):  # x*k부터 p까지
-            dp[i+1][j] = dp[i+1][j] or dp[i][j-p] 
-            # j-p가 가능하다면 dp[i+1][j] 가능
-
+            dp[i][j] = dp[i][j] or dp[i-1][j-p] 
+            # j-p가 가능하다면 dp[i][j] 가능
     dp[1][p] = 1  # 첫 번째 아이템에 대해서는 p를 가능하게 설정
 
 
+
 answer = 0
-for i in range(x * k + 1):
+for i in range(x*k + 1):
     if dp[k][i]:
         answer = max(answer, i*(x*k - i))
 # print(dp)
